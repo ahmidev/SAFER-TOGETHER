93302d90a0eb2f7e19b1e2698ef3a380eebfaa29
ahmidev <ahmid.aitouali@gmail>
2023-03-17 10:47:28 +0100

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ProfilComponent } from './profil/profil.component';
+import { MapComponent } from './map/map.component';
+import { DiscussionComponent } from './discussion/discussion.component';
+import { MessageComponent } from './message/message.component';
+import { SaferListComponent } from './safer-list/safer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    ProfilComponent,
+    MapComponent,
+    DiscussionComponent,
+    MessageComponent,
+    SaferListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/safer-list/safer-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-safer-list',
+  templateUrl: './safer-list.component.html',
+  styleUrls: ['./safer-list.component.css']
+})
+export class SaferListComponent {
+
+}
