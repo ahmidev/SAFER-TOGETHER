7b216a14ebd04459e7fea38ab08932d874373e18
himedsoso <himed.abidar@gmail.com>
2023-03-20 14:50:47 +0100

    toto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfilComponent } from './profil/profil.component';
+
 
 const routes: Routes = [
-  {path: 'profil', component: ProfilComponent}
+  
 ];
 
 @NgModule({
