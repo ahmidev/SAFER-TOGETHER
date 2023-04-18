bf2daaeabf7725edfd4209b99f8cdf13eff2909d
BA\_MUSTAPHA <pro.mustapha@gmail.com>
2023-04-18 14:54:46 +0200

    geolocation

--- src/app/parent-map/parent-map.component.ts
+++ src/app/parent-map/parent-map.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PositionService } from '../Services/position.service';
 
 @Component({
   selector: 'app-parent-map',
   templateUrl: './parent-map.component.html',
   styleUrls: ['./parent-map.component.css']
 })
-export class ParentMapComponent {
+export class ParentMapComponent implements OnInit {
+
+
+  constructor(private positionService: PositionService,) {
+
+  }
+
+
+  ngOnInit(): void {
+
+    this.positionService.getLocation().subscribe(geolocations => {
+      // this.geolocations.push(geolocations)
+      console.log(geolocations)
+    })
+  }
 
 }
